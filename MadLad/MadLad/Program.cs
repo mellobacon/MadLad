5a0508abf6c3719374b3eaf024bebe13a1ea0523
mellobacon <liz6757@outlook.com>
2021-03-14 23:46:55 -0500

    Fixed REPL from being dumb

--- MadLad/MadLad/Program.cs
+++ MadLad/MadLad/Program.cs
@@ -19,70 +19,76 @@ namespace MadLad
                 {
                     return;
                 }
-                ProcessCommand(input);
-
-                // blah blah compiler stuff
-                var Lexer = new Lexer.Lexer(input);
-                while (true)
+                if (input.StartsWith("#"))
+                    ProcessCommand(input);
+                else
                 {
-                    var errors = Lexer.Errors;
-                    var token = Lexer.Lex();
-                    if (token.Kind == SyntaxKind.EOFToken)
+                    // blah blah compiler stuff
+                    var Lexer = new Lexer.Lexer(input);
+                    while (true)
                     {
-                        Console.WriteLine();
-                        break;
-                    }
-
-                    if (showbasiclexer)
-                    {
-                        if (token.Kind != SyntaxKind.WhitespaceToken && token.Value != null)
+                        var errors = Lexer.Errors;
+                        var token = Lexer.Lex();
+                        if (token.Kind == SyntaxKind.EOFToken)
                         {
-                            Console.Write($"[{token.Kind}:{token.Value}]");   
-                        }
-                        else if (token.Kind != SyntaxKind.WhitespaceToken)
-                        {
-                            Console.Write($"[{token.Kind}]");
-                        }
-                    }
-
-                    if (showfullexer)
-                    {
-                        object value;
-                        if (token.Value == null)
-                        {
-                            value = "";
-                        }
-                        else
-                        {
-                            value = $"({token.Value.GetType()})";
-                        }
-                        Console.WriteLine($"Text:'{token.Text}'---Type:{token.Kind}---Value{value}:{token.Value}");
-                    }
-                    if (errors.Any())
-                    {
-                        foreach (var error in errors)
-                        {
-                            var prefix = input.Substring(0, error.Span.Start);
-                            var occurance = input.Substring(error.Span.Start, error.Span.Length);
-                            var suffix = input.Substring(error.Span.End);
-
-                            // Print the message
-                            Console.Write($"{error.Details} at: ");
-                            
-                            // Print what is before the error
-                            Console.WriteLine("   ");
-                            Console.Write(prefix);
-                        
-                            // Print where the error occurs
-                            Console.ForegroundColor = ConsoleColor.DarkRed;
-                            Console.Write(occurance);
-                            Console.ResetColor();
-                        
-                            // Print what is after the error
-                            Console.Write(suffix);
                             Console.WriteLine();
+                            break;
                         }
-                        break;
+
+                        if (showbasiclexer)
+                        {
+                            if (token.Kind != SyntaxKind.WhitespaceToken && token.Value != null)
+                            {
+                                Console.Write($"[{token.Kind}:{token.Value}]");   
+                            }
+                            else if (token.Kind != SyntaxKind.WhitespaceToken)
+                            {
+                                Console.Write($"[{token.Kind}]");
+                            }
+                        }
+
+                        if (showfullexer)
+                        {
+                            object value;
+                            if (token.Value == null)
+                            {
+                                value = "";
+                            }
+                            else
+                            {
+                                value = $"({token.Value.GetType()})";
+                            }
+                            Console.WriteLine($"Text:'{token.Text}'---Type:{token.Kind}---Value{value}:{token.Value}");
+                        }
+                        if (errors.Any())
+                        {
+                            if (showbasiclexer)
+                            {
+                                Console.WriteLine();
+                            }
+                            foreach (var error in errors)
+                            {
+                                var prefix = input.Substring(0, error.Span.Start);
+                                var occurance = input.Substring(error.Span.Start, error.Span.Length);
+                                var suffix = input.Substring(error.Span.End);
+                                // Print the message
+                                Console.Write($"{error.Details} at: ");
+                                
+                                // Print what is before the error
+                                Console.WriteLine("   ");
+                                Console.Write(prefix);
+                            
+                                // Print where the error occurs
+                                Console.ForegroundColor = ConsoleColor.DarkRed;
+                                Console.Write(occurance);
+                                Console.ResetColor();
+                            
+                                // Print what is after the error
+                                Console.Write(suffix);
+                                Console.WriteLine();
+                            }
+                            break;
+                        }
                     }
                 }
             }
