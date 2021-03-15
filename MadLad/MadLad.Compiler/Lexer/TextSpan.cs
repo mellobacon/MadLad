1e1fcf387684a7d46b1029ac62c2e9175abd310f
mellobacon <liz6757@outlook.com>
2021-03-14 23:15:24 -0500

    Added TextSpan

---
+++ MadLad/MadLad.Compiler/Lexer/TextSpan.cs
@@ -0,0 +1,15 @@
+﻿namespace MadLad.Lexer
+{
+    public class TextSpan
+    {
+        public readonly int Start;
+        public readonly int Length;
+        public int End => Start + Length;
+
+        public TextSpan(int start, int length)
+        {
+            Start = start;
+            Length = length;
+        }
+    }
+}
