087555abfa0836e555f3f7030f88f2d4a6657416
mellobacon <liz6757@outlook.com>
2021-03-17 21:08:29 -0500

    Added syntax precedences

---
+++ MadLad/MadLad.Compiler/Syntax/SyntaxPrecedences.cs
@@ -0,0 +1,18 @@
+﻿namespace MadLad.MadLad.Compiler.Syntax
+{
+    public static class SyntaxPrecedences
+    {
+        public static int GetBinaryOperatorPrecedence( this SyntaxKind kind)
+        {
+            // return binary operator precedence
+            return kind switch
+            {
+                SyntaxKind.StarToken => 4,
+                SyntaxKind.SlashToken => 3,
+                SyntaxKind.PlusToken => 2,
+                SyntaxKind.MinusToken => 1,
+                _ => 0
+            };
+        }
+    }
+}
