0e102a9b1b516720c4223499cfc68775da43523f
mellobacon <liz6757@outlook.com>
2021-03-17 21:08:41 -0500

    Added syntax tree

---
+++ MadLad/MadLad.Compiler/Syntax/SyntaxTree.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using MadLad.MadLad.Compiler.ErrorReporting;
+using MadLad.MadLad.Compiler.Syntax.Expressions;
+
+namespace MadLad.MadLad.Compiler.Syntax
+{
+    public class SyntaxTree
+    {
+        public readonly IEnumerable<Error> Errors;
+        public readonly ExpressionSyntax Root;
+        public readonly SyntaxToken Eoftoken;
+
+        public SyntaxTree(IEnumerable<Error> errors, ExpressionSyntax root, SyntaxToken eoftoken)
+        {
+            Errors = errors.ToArray();
+            Root = root;
+            Eoftoken = eoftoken;
+        }
+
+        public static SyntaxTree Parse(string text)
+        {
+            var parser = new Parser.Parser(text);
+            return parser.Parse();
+        }
+    }
+}
