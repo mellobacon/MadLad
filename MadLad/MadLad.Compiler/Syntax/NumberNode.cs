9afa3350c5eac52bd4aa7c89660ec699c7c99194
mellobacon <liz6757@outlook.com>
2021-03-17 21:08:01 -0500

    Added number node

---
+++ MadLad/MadLad.Compiler/Syntax/NumberNode.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using MadLad.MadLad.Compiler.Syntax.Expressions;
+
+namespace MadLad.MadLad.Compiler.Syntax
+{
+    public class NumberNode : ExpressionSyntax
+    {
+        SyntaxToken Token { get; }
+        public override SyntaxKind Kind => SyntaxKind.LiteralExpression;
+        public override IEnumerable<SyntaxNode> GetChildren()
+        {
+            yield return Token;
+        }
+
+        public NumberNode(SyntaxToken token)
+        {
+            Token = token;
+        }
+    }
+}
