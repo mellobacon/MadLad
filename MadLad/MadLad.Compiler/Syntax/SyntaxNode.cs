10009675b630e4bf5354fd568cd35935fa31703d
mellobacon <liz6757@outlook.com>
2021-03-17 21:08:13 -0500

    Added syntax node

---
+++ MadLad/MadLad.Compiler/Syntax/SyntaxNode.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace MadLad.MadLad.Compiler.Syntax
+{
+    public abstract class SyntaxNode
+    {
+        public abstract SyntaxKind Kind { get; }
+
+        public abstract IEnumerable<SyntaxNode> GetChildren();
+    }
+}
