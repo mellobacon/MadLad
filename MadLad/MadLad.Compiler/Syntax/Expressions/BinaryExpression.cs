ba95cf8733fbc4500a56dbafd131ee864e729487
mellobacon <liz6757@outlook.com>
2021-03-17 21:08:54 -0500

    Added binary expression

---
+++ MadLad/MadLad.Compiler/Syntax/Expressions/BinaryExpression.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace MadLad.MadLad.Compiler.Syntax.Expressions
+{
+    public class BinaryExpression : ExpressionSyntax
+    {
+        readonly ExpressionSyntax Left;
+        readonly SyntaxToken Op;
+        readonly ExpressionSyntax Right;
+        public override SyntaxKind Kind => SyntaxKind.BinaryExpression;
+        public override IEnumerable<SyntaxNode> GetChildren()
+        {
+            yield return Left;
+            yield return Op;
+            yield return Right;
+        }
+
+        public BinaryExpression(ExpressionSyntax left, SyntaxToken op, ExpressionSyntax right)
+        {
+            Left = left;
+            Op = op;
+            Right = right;
+        }
+    }
+}
