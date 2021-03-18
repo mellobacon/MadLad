962480fe8d8740aa22eacbc6973c63cda8e187ca
mellobacon <liz6757@outlook.com>
2021-03-17 20:43:35 -0500

    Added toString override

--- MadLad/MadLad.Compiler/ErrorReporting/Error.cs
+++ MadLad/MadLad.Compiler/ErrorReporting/Error.cs
@@ -1,15 +1,22 @@
-﻿namespace MadLad.MadLad.Compiler.ErrorReporting
+﻿using MadLad.MadLad.Compiler.Syntax.Text;
+
+namespace MadLad.MadLad.Compiler.ErrorReporting
 {
     public class Error
     {
         public readonly TextSpan Span;
-        public readonly string Details;
+        readonly string Details;
 
         // Defines what makes up an error message
         public Error(TextSpan span, string details)
         {
             Span = span;
             Details = details;
+        }
+
+        public override string ToString()
+        {
+            return Details;
         }
     }
 }
