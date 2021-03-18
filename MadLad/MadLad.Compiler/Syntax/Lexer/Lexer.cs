d5bbdb491044a1074ec790f83173635891682abe
mellobacon <liz6757@outlook.com>
2021-03-17 20:44:38 -0500

    Reorganized lexer

--- MadLad/MadLad.Compiler/Syntax/Lexer/Lexer.cs
+++ MadLad/MadLad.Compiler/Syntax/Lexer/Lexer.cs
@@ -1,6 +1,6 @@
 ﻿using MadLad.MadLad.Compiler.ErrorReporting;
 
-namespace MadLad.MadLad.Compiler.Lexer
+namespace MadLad.MadLad.Compiler.Syntax.Lexer
 {
     public class Lexer
     {
@@ -14,71 +14,11 @@ namespace MadLad.MadLad.Compiler.Lexer
 
         readonly ErrorList ErrorList = new();
         public ErrorList Errors => ErrorList;
-        
+
         public Lexer(string text)
         {
             Text = text;
         }
-
-        #region Operations for lexing
-
-        private void Advance()
-        {
-            Position++;
-        }
-        
-        // returns the char at whatever position depending on the offset
-        private char Peek(int offset)
-        {
-            var index = Position + offset;
-            return index >= Text.Length ? '\0' : Text[index];
-        }
-
-        private void ReadNumberToken()
-        {
-            var dotcount = 0;
-            while (char.IsDigit(Current) || Current == '.')
-            {
-                if (Current == '.')
-                {
-                    if (dotcount == 1)
-                    {
-                        break;
-                    }
-                    dotcount++;
-                }
-                Advance();
-            }
-
-            var length = Position - Start;
-            var text = Text.Substring(Start, length);
-            _text = text;
-            Kind = SyntaxKind.NumberToken;
-
-            // if there is a decimal in the number its a float else its an int
-            if (Text.Contains('.') && dotcount == 1)
-            {
-                float.TryParse(_text, out var value);
-                Value = value;
-            }
-            else
-            {
-                int.TryParse(_text, out var value);
-                Value = value;
-            }
-        }
-
-        private void ReadWhiteSpaceToken()
-        {
-            while (char.IsWhiteSpace(Current))
-            {
-                Advance();
-            }
-            _text = " ";
-            Kind = SyntaxKind.WhitespaceToken;
-        }
-
-        #endregion
 
         /// <summary>
         /// Returns each syntax token in the input
@@ -146,10 +86,71 @@ namespace MadLad.MadLad.Compiler.Lexer
                     else
                     {
                         ErrorList.ReportBadCharacter(Position, Current);
+                        Advance();
                     }
                     break;
             }
-            return new SyntaxToken(_text, Kind, Value);
+            return new SyntaxToken(_text, Kind, Value, Position);
         }
+        
+        #region Operations for lexing
+
+        private void Advance()
+        {
+            Position++;
+        }
+        
+        // returns the char at whatever position depending on the offset
+        private char Peek(int offset)
+        {
+            var index = Position + offset;
+            return index >= Text.Length ? '\0' : Text[index];
+        }
+
+        private void ReadNumberToken()
+        {
+            var dotcount = 0;
+            while (char.IsDigit(Current) || Current == '.')
+            {
+                if (Current == '.')
+                {
+                    if (dotcount == 1)
+                    {
+                        break;
+                    }
+                    dotcount++;
+                }
+                Advance();
+            }
+
+            var length = Position - Start;
+            var text = Text.Substring(Start, length);
+            _text = text;
+            Kind = SyntaxKind.NumberToken;
+
+            // if there is a decimal in the number its a float else its an int
+            if (Text.Contains('.') && dotcount == 1)
+            {
+                float.TryParse(_text, out var value);
+                Value = value;
+            }
+            else
+            {
+                int.TryParse(_text, out var value);
+                Value = value;
+            }
+        }
+
+        private void ReadWhiteSpaceToken()
+        {
+            while (char.IsWhiteSpace(Current))
+            {
+                Advance();
+            }
+            _text = " ";
+            Kind = SyntaxKind.WhitespaceToken;
+        }
+
+        #endregion
     }
 }
