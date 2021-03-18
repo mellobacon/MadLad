ee4df493136d201e54803d4d9a59d6d0c2b4f961
mellobacon <liz6757@outlook.com>
2021-03-17 20:57:34 -0500

    Did stuff

---
+++ MadLad/MadLad.Compiler/Syntax/SyntaxToken.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using MadLad.MadLad.Compiler.Syntax.Text;
+
+namespace MadLad.MadLad.Compiler.Syntax
+{
+    public class SyntaxToken : SyntaxNode
+    {
+        public readonly string Text;
+        public override SyntaxKind Kind { get; }
+        public readonly object Value;
+        public readonly int Position;
+        public TextSpan Span => new(Position, Text.Length);
+        
+        /// <summary>
+        /// Defines what a syntax token is ie what components make up a syntax token.
+        /// </summary>
+        /// <param name="text"></param>
+        /// <param name="kind"></param>
+        /// <param name="value"></param>
+        /// <param name="position"></param>
+        public SyntaxToken(string text, SyntaxKind kind, object value, int position)
+        {
+            Text = text;
+            Kind = kind;
+            Value = value;
+            Position = position;
+        }
+
+        public override IEnumerable<SyntaxNode> GetChildren()
+        {
+            return Enumerable.Empty<SyntaxNode>();
+        }
+    }
+}
