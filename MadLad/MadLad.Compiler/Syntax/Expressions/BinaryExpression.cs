5ab8188e2bdb77c1ba53c2a0f54b38cf78bdd2fb
mellobacon <liz6757@outlook.com>
2021-03-19 18:23:17 -0500

    Reordered stuff

--- MadLad/MadLad.Compiler/Syntax/Expressions/BinaryExpression.cs
+++ MadLad/MadLad.Compiler/Syntax/Expressions/BinaryExpression.cs
@@ -7,19 +7,20 @@ namespace MadLad.MadLad.Compiler.Syntax.Expressions
         readonly ExpressionSyntax Left;
         readonly SyntaxToken Op;
         readonly ExpressionSyntax Right;
-        public override SyntaxKind Kind => SyntaxKind.BinaryExpression;
-        public override IEnumerable<SyntaxNode> GetChildren()
-        {
-            yield return Left;
-            yield return Op;
-            yield return Right;
-        }
 
         public BinaryExpression(ExpressionSyntax left, SyntaxToken op, ExpressionSyntax right)
         {
             Left = left;
             Op = op;
             Right = right;
+        }
+        
+        public override SyntaxKind Kind => SyntaxKind.BinaryExpression;
+        public override IEnumerable<SyntaxNode> GetChildren()
+        {
+            yield return Left;
+            yield return Op;
+            yield return Right;
         }
     }
 }
