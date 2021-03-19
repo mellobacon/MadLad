a9ecc8377328467af9debd13dacbdc501b98e792
mellobacon <liz6757@outlook.com>
2021-03-19 18:22:24 -0500

    Added grouped expression

---
+++ MadLad/MadLad.Compiler/Syntax/Expressions/GroupedExpression.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace MadLad.MadLad.Compiler.Syntax.Expressions
+{
+    public class GroupedExpression : ExpressionSyntax
+    {
+        public SyntaxToken Openparentoken;
+        public ExpressionSyntax Expression;
+        public SyntaxToken Closedparentoken;
+
+        public GroupedExpression(SyntaxToken openparentoken, ExpressionSyntax expression, SyntaxToken closedparentoken)
+        {
+            Openparentoken = openparentoken;
+            Expression = expression;
+            Closedparentoken = closedparentoken;
+        }
+
+        public override SyntaxKind Kind => SyntaxKind.GroupedExpression;
+        public override IEnumerable<SyntaxNode> GetChildren()
+        {
+            yield return Openparentoken;
+            yield return Expression;
+            yield return Closedparentoken;
+        }
+    }
+}
