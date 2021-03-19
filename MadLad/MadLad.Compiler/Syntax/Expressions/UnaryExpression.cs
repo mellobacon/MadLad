907b8575a2b78f123ec4b50a712b628b705cc730
mellobacon <liz6757@outlook.com>
2021-03-19 18:22:36 -0500

    Added unary expression

---
+++ MadLad/MadLad.Compiler/Syntax/Expressions/UnaryExpression.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace MadLad.MadLad.Compiler.Syntax.Expressions
+{
+    public class UnaryExpression : ExpressionSyntax
+    {
+        public readonly SyntaxToken OpToken;
+        public readonly ExpressionSyntax Operand;
+        
+        public UnaryExpression(SyntaxToken optoken, ExpressionSyntax operand)
+        {
+            OpToken = optoken;
+            Operand = operand;
+        }
+
+        public override SyntaxKind Kind => SyntaxKind.UnaryExpression;
+        public override IEnumerable<SyntaxNode> GetChildren()
+        {
+            yield return OpToken;
+            yield return Operand;
+        }
+    }
+}
