374ebbbeddaa8843f4b0aced6d4289ebb641214f
mellobacon <42365887+mellobacon@users.noreply.github.com>
2021-03-23 14:34:54 -0500

    Added support for variables, assignments, and grouped expressions

--- MadLad/MadLad.Compiler/Binding/Binder.cs
+++ MadLad/MadLad.Compiler/Binding/Binder.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using MadLad.MadLad.Compiler.Binding.Expressions;
 using MadLad.MadLad.Compiler.ErrorReporting;
 using MadLad.MadLad.Compiler.Syntax;
@@ -8,8 +10,14 @@ namespace MadLad.MadLad.Compiler.Binding
 {
     public class Binder
     {
+        private readonly Dictionary<Variable, object> Variables;
         private readonly ErrorList ErrorList = new();
         public ErrorList Errors => ErrorList;
+
+        public Binder(Dictionary<Variable, object> variables)
+        {
+            Variables = variables;
+        }
         
         public BoundExpression BindExpression(ExpressionSyntax syntax)
         {
@@ -18,6 +26,9 @@ namespace MadLad.MadLad.Compiler.Binding
                 SyntaxKind.LiteralExpression => BindLiteralExpression((LiteralExpression) syntax),
                 SyntaxKind.BinaryExpression => BindBinaryExpression((BinaryExpression) syntax),
                 SyntaxKind.UnaryExpression => BindUnaryExpression((UnaryExpression) syntax),
+                SyntaxKind.GroupedExpression => BindGroupedExpression((GroupedExpression)syntax),
+                SyntaxKind.AssignmentExpression => BindAssignmentExpression((AssignmentExpression)syntax),
+                SyntaxKind.NameExpression => BindNameExpression((NameExpression)syntax),
                 _ => throw new Exception($"Unexpected syntax {syntax.Kind}")
             };
         }
@@ -51,6 +62,42 @@ namespace MadLad.MadLad.Compiler.Binding
                 return boundoperand;
             }
             return new UnaryBoundExpression(boundoperator, boundoperand);
+        }
+
+        private BoundExpression BindGroupedExpression(GroupedExpression syntax)
+        {
+            return BindExpression(syntax.Expression);
+        }
+
+        private BoundExpression BindAssignmentExpression(AssignmentExpression syntax)
+        {
+            var name = syntax.VariableToken.Text;
+            var expression = BindExpression(syntax.Expression);
+
+            var variable = new Variable(name, expression.Type);
+            
+            var existingvariable = Variables.Keys.FirstOrDefault(v => v.Name == name);
+            if (existingvariable != null)
+            {
+                Variables.Remove(existingvariable);
+            }
+            
+            Variables[variable] = null;
+            
+            return new AssignmentBoundExpression(variable, expression);
+        }
+
+        private BoundExpression BindNameExpression(NameExpression syntax)
+        {
+            var name = syntax.IdentifierToken.Text;
+            var variable = Variables.Keys.FirstOrDefault(v => v.Name == name);
+            if (variable == null)
+            {
+                ErrorList.ReportUndefinedName(syntax.IdentifierToken.Span, name);
+                return new LiteralBoundExpression(0);
+            }
+            
+            return new VariableBoundExpression(variable);
         }
     }
 }
