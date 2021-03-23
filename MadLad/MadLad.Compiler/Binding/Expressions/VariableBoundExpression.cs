8e0a8c6fedf2702d083b273f48d90c810fe9c19e
mellobacon <42365887+mellobacon@users.noreply.github.com>
2021-03-23 14:33:45 -0500

    Added some expressions for variables

---
+++ MadLad/MadLad.Compiler/Binding/Expressions/VariableBoundExpression.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using MadLad.MadLad.Compiler.Syntax;
+
+namespace MadLad.MadLad.Compiler.Binding.Expressions
+{
+    public class VariableBoundExpression : BoundExpression
+    {
+        public readonly Variable Variable;
+        public override Type Type => Variable.Type;
+
+        public VariableBoundExpression(Variable variable)
+        {
+            Variable = variable;
+        }
+
+        public override BoundKind Kind => BoundKind.VariableExpression;
+    }
+}
