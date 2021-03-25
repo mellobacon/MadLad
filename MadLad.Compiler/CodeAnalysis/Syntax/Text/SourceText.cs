9957c021d7088d1e7ffa9dcc219038ef4f18b60e
mellobacon <42365887+mellobacon@users.noreply.github.com>
2021-03-25 12:45:17 -0500

    Added text stuff

---
+++ MadLad.Compiler/CodeAnalysis/Syntax/Text/SourceText.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections.Immutable;
+
+namespace MadLad.Compiler.CodeAnalysis.Syntax.Text
+{
+    public class SourceText
+    {
+        private readonly string Text;
+        public ImmutableArray<TextLine> Lines;
+        public SourceText(string text)
+        {
+            Text = text;
+            Lines = ParseLines(this, text);
+        }
+        
+        private static ImmutableArray<TextLine> ParseLines(SourceText sourcetext, string text)
+        {
+            var result = ImmutableArray.CreateBuilder<TextLine>();
+            var position = 0;
+            var start = 0;
+            while (position < text.Length)
+            {
+                var linebreakwidth = GetLineBreakWidth(text, position);
+                if (linebreakwidth == 0)
+                {
+                    position++;
+                }
+                else
+                {
+                    AddLine(sourcetext, position, start, linebreakwidth, result);
+                    position += linebreakwidth;
+                    start = position;
+                }
+            }
+
+            if (position > start)
+            {
+                AddLine(sourcetext, position, start, 0, result);
+            }
+
+            return result.ToImmutable();
+        }
+        
+        private static int GetLineBreakWidth(string text, int i)
+        {
+            var character = text[i];
+            var length = i + 1 > text.Length ? '\0' : text[i + 1];
+            if (character == '\r' && length == '\n')
+            {
+                return 2;
+            }
+
+            if (character == '\r' || character == '\r')
+            {
+                return 1;
+            }
+            return 0;
+        }
+
+        private static void AddLine(SourceText text, int position, int start, int linebreakwidth, ImmutableArray<TextLine>.Builder result)
+        {
+            var linelength = position - start;
+            var linelengthwithbreak = linelength + linebreakwidth;
+            var line = new TextLine(text, start, linelength, linelengthwithbreak);
+            result.Add(line);
+        }
+
+        public int GetLineIndex(int position)
+        {
+            int lower = 0;
+            int upper = Text.Length - 1;
+            while (lower <= upper)
+            {
+                var index = (lower + upper) / 2;
+                var start = Lines[index].Start;
+                if (position == start)
+                {
+                    return ++position;
+                }
+                else if (start < position)
+                {
+                    upper = index - 1;
+                }
+                else
+                {
+                    lower = index + 1;
+                }
+            }
+
+            return lower - 1;
+        }
+
+        public static SourceText From(string text)
+        {
+            return new SourceText(text);
+        }
+
+        public override string ToString() => Text;
+        public string ToString(int start, int length) => Text.Substring(start, length);
+        public string ToString(TextSpan span) => ToString(span.Start, span.Length);
+    }
+}
---
+++ MadLad.Compiler/CodeAnalysis/Syntax/Text/TextLine.cs
@@ -0,0 +1,25 @@
+﻿namespace MadLad.Compiler.CodeAnalysis.Syntax.Text
+{
+    public class TextLine
+    {
+        private readonly SourceText Text;
+        public readonly int Start;
+        private readonly int Length;
+        private readonly int LengthWithBreak;
+        private int End => Start + Length;
+        public TextSpan Span => new TextSpan(Start, Length);
+        public TextSpan SpanWithBreak => new TextSpan(Start, LengthWithBreak);
+
+        public TextLine(SourceText text, int start, int length, int lengthwithbreak)
+        {
+            Text = text;
+            Start = start;
+            Length = length;
+            LengthWithBreak = lengthwithbreak;
+        }
+        
+        public override string ToString() => Text.ToString(Start, LengthWithBreak);
+        public string ToString(int start, int length) => Text.ToString(Span);
+        public string ToString(TextSpan span) => ToString(span.Start, span.Length);
+    }
+}
