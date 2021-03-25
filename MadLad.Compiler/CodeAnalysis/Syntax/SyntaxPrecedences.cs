0e20b29a0cf95c4cfd70f3b9869999e8b81a7a68
mellobacon <42365887+mellobacon@users.noreply.github.com>
2021-03-25 18:23:22 -0500

    Added stuff and updated precedences

--- MadLad.Compiler/CodeAnalysis/Syntax/SyntaxPrecedences.cs
+++ MadLad.Compiler/CodeAnalysis/Syntax/SyntaxPrecedences.cs
@@ -7,8 +7,8 @@
         {
             return kind switch
             {
-                SyntaxKind.BangToken => 4,
-                SyntaxKind.MinusToken => 4,
+                SyntaxKind.BangToken => 6,
+                SyntaxKind.MinusToken => 6,
                 _ => 0
             };
         }
@@ -18,12 +18,14 @@
             // return binary operator precedence
             return kind switch
             {
-                SyntaxKind.StarToken => 3,
-                SyntaxKind.SlashToken => 3,
-                SyntaxKind.PlusToken => 2,
-                SyntaxKind.MinusToken => 2,
-                SyntaxKind.EqualsEqualsToken => 1,
-                SyntaxKind.NotEqualsToken => 1,
+                SyntaxKind.StarToken => 5,
+                SyntaxKind.SlashToken => 5,
+                SyntaxKind.PlusToken => 4,
+                SyntaxKind.MinusToken => 4,
+                SyntaxKind.EqualsEqualsToken => 3,
+                SyntaxKind.NotEqualsToken => 3,
+                SyntaxKind.AndAmpersandToken => 2,
+                SyntaxKind.OrPipeToken => 1,
                 _ => 0
             };
         }
@@ -48,6 +50,13 @@
                 SyntaxKind.SlashToken => "/",
                 SyntaxKind.OpenParenToken => "(",
                 SyntaxKind.CloseParenToken => ")",
+                SyntaxKind.HatToken => "^",
+                SyntaxKind.BangToken => "!",
+                SyntaxKind.EqualsToken => "=",
+                SyntaxKind.NotEqualsToken => "!=",
+                SyntaxKind.EqualsEqualsToken => "==",
+                SyntaxKind.OrPipeToken => "||",
+                SyntaxKind.AndAmpersandToken => "&&",
                 SyntaxKind.TrueKeyword => "true",
                 SyntaxKind.FalseKeyword => "false",
                 _ => null
