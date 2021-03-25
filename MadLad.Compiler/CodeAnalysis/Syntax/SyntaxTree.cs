7067dbcd96ed07920286682c8d3a85cf8c1ca7be
mellobacon <42365887+mellobacon@users.noreply.github.com>
2021-03-25 14:05:23 -0500

    Changed from parsing normal text to sourcetext

--- MadLad.Compiler/CodeAnalysis/Syntax/SyntaxTree.cs
+++ MadLad.Compiler/CodeAnalysis/Syntax/SyntaxTree.cs
@@ -2,27 +2,32 @@
 using System.Linq;
 using MadLad.Compiler.CodeAnalysis.ErrorReporting;
 using MadLad.Compiler.CodeAnalysis.Syntax.Expressions;
+using MadLad.Compiler.CodeAnalysis.Syntax.Text;
 
 namespace MadLad.Compiler.CodeAnalysis.Syntax
 {
     public class SyntaxTree
     {
         public readonly IEnumerable<Error> Errors;
+        public readonly SourceText Text;
         public readonly ExpressionSyntax Root;
         private readonly SyntaxToken Eoftoken;
 
-        public SyntaxTree(IEnumerable<Error> errors, ExpressionSyntax root, SyntaxToken eoftoken)
+        public SyntaxTree(SourceText text, IEnumerable<Error> errors, ExpressionSyntax root, SyntaxToken eoftoken)
         {
             Errors = errors.ToArray();
+            Text = text;
             Root = root;
             Eoftoken = eoftoken;
         }
 
         public static SyntaxTree Parse(string text)
         {
-            // lex the input
-            var parser = new Parser.Parser(text);
-            // parse the input
+            // Convert the text into sourcetext
+            var sourcetext = SourceText.From(text);
+            // Lex the input
+            var parser = new Parser.Parser(sourcetext);
+            // Parse the input
             return parser.Parse();
         }
     }
