5f62e77f9b5c5bc63fbbe8d09fdecbbebb2d14ad
mellobacon <liz6757@outlook.com>
2021-04-05 19:18:53 -0500

    added bound scope

---
+++ MadLad.Compiler/CodeAnalysis/Binding/BoundScope.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Collections.Immutable;
+using MadLad.Compiler.CodeAnalysis.Syntax;
+
+namespace MadLad.Compiler.CodeAnalysis.Binding
+{
+    public sealed class BoundScope
+    {
+        readonly BoundScope Parent;
+        Dictionary<string, Variable> variables = new();
+
+        public BoundScope(BoundScope parent)
+        {
+            Parent = parent;
+        }
+
+        public bool TryDeclare(Variable variable)
+        {
+            // checks if the variable is declared. if it is, dont redeclare it
+            if (variables.ContainsKey(variable.Name))
+            {
+                return false;
+            }
+            variables.Add(variable.Name, variable);
+            return true;
+        }
+
+        public bool TryLookup(string name, out Variable variable)
+        {
+            // checks if the declared variable has a value
+            if (variables.TryGetValue(name, out variable))
+            {
+                return true;
+            }
+
+            if (Parent == null)
+            {
+                return false;
+            }
+
+            return Parent.TryLookup(name, out variable);
+        }
+
+        public ImmutableArray<Variable> GetDeclaredVariables()
+        {
+            return variables.Values.ToImmutableArray();
+        }
+    }
+}
