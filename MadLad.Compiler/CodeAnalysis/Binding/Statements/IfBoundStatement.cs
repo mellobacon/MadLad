ed99d92e4df44fc38206726f66883371d6218729
mellobacon <liz6757@outlook.com>
2021-04-11 10:03:56 -0500

    added binded if statement

---
+++ MadLad.Compiler/CodeAnalysis/Binding/Statements/IfBoundStatement.cs
@@ -0,0 +1,19 @@
+﻿using MadLad.Compiler.CodeAnalysis.Binding.Expressions;
+
+namespace MadLad.Compiler.CodeAnalysis.Binding.Statements
+{
+    public class IfBoundStatement : BoundStatement
+    {
+        public readonly BoundExpression Conditiion;
+        public readonly BoundStatement Statement;
+        public readonly BoundStatement Elsestatement;
+
+        public IfBoundStatement(BoundExpression conditiion, BoundStatement statement, BoundStatement elsestatement)
+        {
+            Conditiion = conditiion;
+            Statement = statement;
+            Elsestatement = elsestatement;
+        }
+        public override BoundKind Kind => BoundKind.IfStatement;
+    }
+}
