d900d176a80b75506748f7b720f9dc1eb75c7e26
mellobacon <liz6757@outlook.com>
2021-04-11 10:06:26 -0500

    added if statement

---
+++ MadLad.Compiler/CodeAnalysis/Syntax/Statements/IfStatement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using MadLad.Compiler.CodeAnalysis.Syntax.Expressions;
+
+namespace MadLad.Compiler.CodeAnalysis.Syntax.Statements
+{
+    public class IfStatement : StatementSyntax
+    {
+        readonly SyntaxToken Ifkeyword;
+        public readonly ExpressionSyntax Condition;
+        public readonly StatementSyntax Thenstatement;
+        public readonly ElseStatement Elsestatement;
+
+        // if ()
+        // {
+        //
+        // }
+        public IfStatement(SyntaxToken ifkeyword, ExpressionSyntax condition, StatementSyntax thenstatement,
+            ElseStatement elsestatement)
+        {
+            Ifkeyword = ifkeyword;
+            Condition = condition;
+            Thenstatement = thenstatement;
+            Elsestatement = elsestatement;
+        }
+        
+        public override SyntaxKind Kind => SyntaxKind.IfStatement;
+        public override IEnumerable<SyntaxNode> GetChildren()
+        {
+            yield return Ifkeyword;
+            yield return Condition;
+            yield return Thenstatement;
+            yield return Elsestatement;
+        }
+    }
+}
