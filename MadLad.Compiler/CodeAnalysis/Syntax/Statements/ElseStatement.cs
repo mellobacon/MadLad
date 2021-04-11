49537e1a7343919219347af8849c7cc336a0395a
mellobacon <liz6757@outlook.com>
2021-04-11 10:06:07 -0500

    added else clause

---
+++ MadLad.Compiler/CodeAnalysis/Syntax/Statements/ElseStatement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace MadLad.Compiler.CodeAnalysis.Syntax.Statements
+{
+    public class ElseStatement : StatementSyntax
+    {
+        private readonly SyntaxToken Elsekeyword;
+        public readonly StatementSyntax Elsestatement;
+
+        public ElseStatement(SyntaxToken elsekeyword, StatementSyntax elsestatement)
+        {
+            Elsekeyword = elsekeyword;
+            Elsestatement = elsestatement;
+        }
+
+        public override SyntaxKind Kind => SyntaxKind.ElseStatement;
+        public override IEnumerable<SyntaxNode> GetChildren()
+        {
+            yield return Elsekeyword;
+            yield return Elsestatement;
+        }
+    }
+}
