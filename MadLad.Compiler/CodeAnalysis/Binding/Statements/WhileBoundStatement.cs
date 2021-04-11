eb9ed16f30165760630a0d90a81d33ce2b01577f
mellobacon <liz6757@outlook.com>
2021-04-11 12:18:40 -0500

    added bound while loop

---
+++ MadLad.Compiler/CodeAnalysis/Binding/Statements/WhileBoundStatement.cs
@@ -0,0 +1,17 @@
+﻿using MadLad.Compiler.CodeAnalysis.Binding.Expressions;
+
+namespace MadLad.Compiler.CodeAnalysis.Binding.Statements
+{
+    public class WhileBoundStatement : BoundStatement
+    {
+        public readonly BoundExpression Condition;
+        public readonly BoundStatement Statement;
+
+        public WhileBoundStatement(BoundExpression condition, BoundStatement statement )
+        {
+            Condition = condition;
+            Statement = statement;
+        }
+        public override BoundKind Kind => BoundKind.WhileStatement;
+    }
+}
