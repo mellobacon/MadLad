76c1ebb692a737fc2f7d0753fc13937b3f4f1594
mellobacon <liz6757@outlook.com>
2021-04-11 12:22:59 -0500

    added while statement

---
+++ MadLad.Compiler/CodeAnalysis/Syntax/Statements/WhileStatement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using MadLad.Compiler.CodeAnalysis.Syntax.Expressions;
+
+namespace MadLad.Compiler.CodeAnalysis.Syntax.Statements
+{
+    public class WhileStatement : StatementSyntax
+    {
+        readonly SyntaxToken Whilekeyword;
+        public readonly ExpressionSyntax Condition;
+        public readonly StatementSyntax Dostatement;
+
+        public WhileStatement(SyntaxToken whilekeyword, ExpressionSyntax condition, StatementSyntax dostatement)
+        {
+            Whilekeyword = whilekeyword;
+            Condition = condition;
+            Dostatement = dostatement;
+        }
+
+        public override SyntaxKind Kind => SyntaxKind.WhileStatement;
+        public override IEnumerable<SyntaxNode> GetChildren()
+        {
+            yield return Whilekeyword;
+            yield return Condition;
+            yield return Dostatement;
+        }
+    }
+}
