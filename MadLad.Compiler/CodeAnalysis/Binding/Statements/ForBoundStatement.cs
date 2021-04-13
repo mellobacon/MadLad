57432586f15ff150fbe800f414d3870972804883
mellobacon <liz6757@outlook.com>
2021-04-13 00:12:52 -0500

    Added bound for statement

---
+++ MadLad.Compiler/CodeAnalysis/Binding/Statements/ForBoundStatement.cs
@@ -0,0 +1,22 @@
+﻿using MadLad.Compiler.CodeAnalysis.Binding.Expressions;
+
+namespace MadLad.Compiler.CodeAnalysis.Binding.Statements
+{
+    public class ForBoundStatement : BoundStatement
+    {
+        public readonly BoundStatement Initialization;
+        public readonly BoundExpression Condition;
+        public readonly BoundExpression Iteration;
+        public readonly BoundStatement Statement;
+
+        public ForBoundStatement(BoundStatement initialization, BoundExpression condition, BoundExpression iteration, BoundStatement statement)
+        {
+            Initialization = initialization;
+            Condition = condition;
+            Iteration = iteration;
+            Statement = statement;
+        }
+
+        public override BoundKind Kind => BoundKind.ForStatement;
+    }
+}
