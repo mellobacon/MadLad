4a0efb37c564721265844759f46d3578c46e0b52
mellobacon <liz6757@outlook.com>
2021-05-01 02:37:49 -0500

    added way to return a single error instead of cascading ones

---
+++ MadLad.Compiler/CodeAnalysis/Binding/Expressions/ErrorBoundExpression.cs
@@ -0,0 +1,10 @@
+﻿using MadLad.Compiler.CodeAnalysis.Syntax.Symbols;
+
+namespace MadLad.Compiler.CodeAnalysis.Binding.Expressions
+{
+    internal class ErrorBoundExpression : BoundExpression
+    {
+        public override BoundKind Kind => BoundKind.ErrorExpression;
+        public override TypeSymbol Type => TypeSymbol.Error;
+    }
+}
